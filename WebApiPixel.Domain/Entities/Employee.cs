d2fdbf1350af94e7a6ba31e19369db12505314b2
Сергеев Евгений <sevg3ny@gmail.com>
2022-03-24 13:44:20 +0300

    Добавлены модели: Сотрудник, СотрудникЗаказ, Хронология заказа

---
+++ WebApiPixel.Domain/Entities/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiPixel.Domain.Entities.Base;
+
+namespace WebApiPixel.Domain.Entities
+{
+    /// <summary>
+    /// Сотрудник
+    /// </summary>
+    public class Employee : EntityBase
+    {
+        /// <summary>
+        /// ФИО сотрудника
+        /// </summary>
+        public string Fio { get; set; }
+
+        /// <summary>
+        /// Логин
+        /// </summary>
+        public string Login { get; set; }
+
+        /// <summary>
+        /// Пароль
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Является ли сотрудник админом
+        /// </summary>
+        public bool isAdmin { get; set; }
+    }
+}
