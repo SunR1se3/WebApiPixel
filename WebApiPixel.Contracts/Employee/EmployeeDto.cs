f9a3156d0a774650552552652bce956a34644d7d
Сергеев Евгений <sevg3ny@gmail.com>
2022-03-24 15:43:41 +0300

    (feature-3-add-dto): добавлены дто для хронологии заказов, сотрудника, СотрудникЗаказ

---
+++ WebApiPixel.Contracts/Employee/EmployeeDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiPixel.Contracts.Employee
+{
+    /// <summary>
+    /// Модель представления сотрудника
+    /// </summary>
+    public class EmployeeDto : DtoBase
+    {
+        /// <summary>
+        /// ФИО сотрудника
+        /// </summary>
+        public string Fio { get; set; }
+
+        /// <summary>
+        /// Логин
+        /// </summary>
+        public string Login { get; set; }
+    }
+}
---
+++ WebApiPixel.Contracts/OrderHronology/OrderHronologyDto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiPixel.Contracts.OrderHronology
+{
+    /// <summary>
+    /// Модель представления хронологии заказа
+    /// </summary>
+    public class OrderHronologyDto : DtoBase
+    {
+        /// <summary>
+        /// Дата создания заказа
+        /// </summary>
+        public DateTime DateCreationOrder { get; set; }
+
+        /// <summary>
+        /// Дата принятия заказа
+        /// </summary>
+        public DateTime DateAcceptionOrder { get; set; }
+
+        /// <summary>
+        /// Дата подготовки к выполнению
+        /// </summary>
+        public DateTime DatePrepare { get; set; }
+
+        /// <summary>
+        /// Дата начала выполнения
+        /// </summary>
+        public DateTime DateProduce { get; set; }
+
+        /// <summary>
+        /// Дата окончания работы
+        /// </summary>
+        public DateTime DateOrderIsReady { get; set; }
+
+        /// <summary>
+        /// id заказа
+        /// </summary>
+        public Guid IdOrder { get; set; }
+    }
+}
